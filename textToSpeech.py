dd6e23f766aedc620286704afb7963c3de7b1881
CKOCKIY <58730782+ckoockiy@users.noreply.github.com>
2023-03-01 14:23:02 -0800

    Update textToSpeech.py

--- textToSpeech.py
+++ textToSpeech.py
@@ -3,10 +3,10 @@ from playsound import playsound  # Necesario para reproducir el archivo de sonid
 
 def convertir_texto_a_voz(texto, idioma="es"):
     # Crea un objeto de audio de gTTS
-    locucion = gTTS(text=texto, lang=idioma, slow=False)
+    audio = gTTS(text=texto, lang=idioma, slow=False)
     
     # Guarda la audio como un archivo mp3
-    locucion.save("locucion.mp3")
+    audio.save("locucion.mp3")
     
     # Reproduce el archivo de sonido
     playsound("locucion.mp3")
